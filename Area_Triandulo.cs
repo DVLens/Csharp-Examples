1fe8c5387f6647bd82e22e22b8792d65a37dc73d
DVLens <114842547+DVLens@users.noreply.github.com>
2022-10-05 22:29:37 -0700

    Add files via upload

---
+++ Area_Triandulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba1
+{
+    internal class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa para calcular el área de un triángulo.");
+            Console.WriteLine("Introduzca el primer número.");
+            float baseT = float.Parse(Console.ReadLine());
+            Console.WriteLine("Introduzca el segundo número");
+            float altuT = float.Parse(Console.ReadLine());
+            Console.WriteLine("Tu Resultado es =" + ((baseT * altuT)/2));
+            //Espera un valor del teclado
+            Console.ReadKey();
+        }
+    }
+}
